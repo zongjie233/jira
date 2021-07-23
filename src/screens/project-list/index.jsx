bf52d79e30acb8184cc6f4a13e0d2eeb7cadb1e5
zongjie233 <840573195@qq.com>
2021-07-23 16:41:00 +0800

    添加useMount.useDebounce两个hook

--- src/screens/project-list/index.jsx
+++ src/screens/project-list/index.jsx
@@ -2,7 +2,7 @@ import { List } from "./list";
 import { SearchPanel } from "./search-panel";
 import { useEffect, useState } from "react";
 import React from "react";
-import { cleanObject } from "utils";
+import { cleanObject, useDebounce, useMount } from "utils";
 import * as qs from "qs";
 
 const apiUrl = process.env.REACT_APP_API_URL;
@@ -13,29 +13,29 @@ export const ProjectListScreen = () => {
   });
 
   const [list, setList] = useState([]);
-
+  const debouncedParam = useDebounce(param, 2000);
   const [users, setUsers] = useState([]);
 
   /* param变化时项目请求列表的接口 */
   useEffect(() => {
     // fetch返回一个promise,所以用then处理
-    fetch(`${apiUrl}/projects?${qs.stringify(cleanObject(param))}`).then(
-      async (response) => {
-        if (response.ok) {
-          setList(await response.json());
-        }
+    fetch(
+      `${apiUrl}/projects?${qs.stringify(cleanObject(debouncedParam))}`
+    ).then(async (response) => {
+      if (response.ok) {
+        setList(await response.json());
       }
-    );
-  }, [param]);
+    });
+  }, [debouncedParam]);
 
-  useEffect(() => {
+  useMount(() => {
     // fetch返回一个promise,所以用then处理
     fetch(`${apiUrl}/users`).then(async (response) => {
       if (response.ok) {
         setUsers(await response.json());
       }
     });
-  }, [param]);
+  });
 
   return (
     <div>
--- src/utils/index.js
+++ src/utils/index.js
@@ -1,3 +1,5 @@
+import { useEffect, useState } from "react";
+
 export const isFalsy = (value) => (value === 0 ? false : !value);
 
 export const cleanObject = (object) => {
@@ -9,4 +11,22 @@ export const cleanObject = (object) => {
     }
   });
   return result;
+};
+
+export const useMount = (callback) => {
+  useEffect(() => {
+    callback();
+  }, []);
+};
+
+export const useDebounce = (value, delay) => {
+  const [debouncedValue, setDebouncedValue] = useState(value);
+
+  useEffect(() => {
+    // 每次再value变化之后设置一个定时器
+    const timeout = setTimeout(() => setDebouncedValue(value), delay);
+    // 每次再上一个effect处理过之后再运行
+    return () => clearTimeout(timeout);
+  }, [value, delay]);
+  return debouncedValue;
 };
