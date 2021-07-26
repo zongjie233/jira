de1c627011887a3ba254d01425ea26afe69fb0b3
zongjie233 <840573195@qq.com>
2021-07-26 14:39:10 +0800

    useDebounce添加泛型

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -21,7 +21,7 @@ export const useMount = (callback: () => void) => {
   }, []);
 };
 
-export const useDebounce = (value: unknown, delay?: number): any => {
+export const useDebounce = <V>(value: V, delay?: number) => {
   const [debouncedValue, setDebouncedValue] = useState(value);
 
   useEffect(() => {
