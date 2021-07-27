2b190253e374837d795ca62bebb954c67e02422f
zongjie233 <840573195@qq.com>
2021-07-27 10:37:39 +0800

    更新form表单

--- src/App.tsx
+++ src/App.tsx
@@ -2,11 +2,15 @@ import React from "react";
 // import logo from "./logo.svg";
 import "./App.css";
 import { ProjectListScreen } from "screens/project-list";
+import { TsReactTest } from "try-use-array";
+import { LoginScreen } from "screens/login";
 
 function App() {
   return (
     <div className="App">
       <ProjectListScreen />
+      <LoginScreen />
+      {/* <TsReactTest /> */}
     </div>
   );
 }
---
+++ src/try-use-array.tsx
@@ -0,0 +1,27 @@
+import React from "react";
+import { useArray, useMount } from "utils";
+
+export const TsReactTest = () => {
+  const persons: { name: string; age: number }[] = [
+    { name: "jack", age: 25 },
+    { name: "ma", age: 22 },
+  ];
+  const { value, clear, removeIndex, add } = useArray(persons);
+  useMount(() => {});
+  return (
+    <div>
+      <button onClick={() => add({ name: "john", age: 22 })}>add john</button>
+      <button onClick={() => removeIndex(0)}>remove 0</button>
+      <button style={{ marginBottom: "50px" }} onClick={() => clear()}>
+        clear
+      </button>
+      {value.map((person, index) => (
+        <div style={{ marginBottom: "30px" }}>
+          <span style={{ color: "red" }}>{index}</span>
+          <span>{person.name}</span>
+          <span>{person.age}</span>
+        </div>
+      ))}
+    </div>
+  );
+};
